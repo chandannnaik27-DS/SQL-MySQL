53910d8e7836b20a2771fa25d7a6126d2745c4b0
Chandan <chandannnaik03@gmail.com>
2026-09-23 21:49:50 +0530

    Deleting data

---
+++ Deleting_data.sql
@@ -0,0 +1,11 @@
+-- DELETING DATA
+
+DELETE FROM users WHERE gender = 'Other';
+SELECT * FROM users;
+
+DELETE  FROM users WHERE salary < 50000;
+SELECT * FROM users ;
+
+DELETE FROM users 
+WHERE salary IS NULL;
+SELECT * FROM users
